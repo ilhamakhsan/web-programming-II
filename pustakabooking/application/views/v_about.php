987e53a96070eebffcad76598f037bd704aa6372
ilhamakhsan <ilhamakhsan23@gmail.com>
2020-10-25 23:28:57 +0700

    pertama commit

---
+++ pustakabooking/application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ilham</li>
+        <li>Nama Belakang : Akhsani</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Brebes, Wanasari..</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Brebes</li>
+    </ul>	
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Memancing</li>
+        <li>Berkebun</li>
+    </ul>	
+</section>
---
+++ pustakabooking/application/views/v_bootstrap.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Load file CSS Bootstrap offline -->
+<link rel="stylesheet" href="http://localhost/pustakabooking/assets/css/bootstrap.min.css">
+<script src="http://localhost/pustakabooking/assets/js/bootstrap.min.css"></script>
+</head>
+
+<body>
+
+<div class="jumbotron text-center">
+    <h1>Halaman CodeIgniter dan Bootstrap</h1>
+    <p>Halaman ini bisa dirapikan menggunakan kode bootstrap</p> 
+    <button type="button" class="btn btn-danger">Home</button>
+    <button type="button" class="btn btn-info">About</button>
+    <button type="button" class="btn btn-info">Contact</button>
+    <a href="https://poltektegal.ac.id" target="_blank" class="btn btn-info" role="button">Link button</a>
+</div>
+  
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <h3>Kolom pertama</h3>
+            <p>Paragraf pertama</p>
+            <p>Paragraf kedua</p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Kolom kedua</h3>
+            <p>Paragraf pertama</p>
+            <p>Paragraf kedua</p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Kolom ketiga</h3>        
+            <p>Paragraf pertama</p>
+            <p>Paragraf kedua</p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+<ul class="pagination justify-content-center" style="margin:20px 0">
+  <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+  <li class="page-item"><a class="page-link" href="#">1</a></li>
+  <li class="page-item"><a class="page-link" href="#">2</a></li>
+  <li class="page-item"><a class="page-link" href="#">3</a></li>
+  <li class="page-item"><a class="page-link" href="#">Next</a></li>
+</ul> 
+</div>
+<!-- <div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="http://localhost/pustakabooking/assets/images/image.jpeg" class="rounded" alt="Cinque Terre"> 
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+        <img src="http://localhost/pustakabooking/assets/images/image.jpeg" class="rounded" alt="Cinque Terre"> 
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="http://localhost/pustakabooking/assets/images/image.jpeg" class="rounded" alt="Cinque Terre"> 
+        </div>
+    </div>
+</div> -->
+
+</body>
+</html>
